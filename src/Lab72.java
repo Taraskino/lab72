c11444087485d04dbe86d4f9cd7adcdef92582e9
Taras <TarasMuzychka1989@gmail.com>
2021-11-27 20:15:47 +0100

    1

---
+++ src/Lab72.java
@@ -0,0 +1,30 @@
+public class Lab72 {
+    public static void main(String[] args) {
+        int n = 2;
+        System.out.println("n = " + n);
+        System.out.println("------");
+        for (int i = 1; i < n + 1; i++){
+            for (int j = 0; j < n; j++){
+                System.out.print(i + j + " ");
+            }
+            System.out.println();
+    }
+         n = 3;
+        System.out.println("\nn = " + n);
+        System.out.println("------");
+        for (int i = 1; i < n + 1; i++){
+            for (int j = 0; j < n; j++){
+                System.out.print(i + j + " ");
+            }
+            System.out.println();
+        }
+         n = 4;
+        System.out.println("\nn = " + n);
+        System.out.println("------");
+        for (int i = 1; i < n + 1; i++){
+            for (int j = 0; j < n; j++){
+                System.out.print(i + j + " ");
+            }
+            System.out.println();
+        }
+}}
